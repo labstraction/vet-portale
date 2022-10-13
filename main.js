5ddcd176d4091e1e4dd61781a902e029515e4154
labstraction <labstraction@protonmail.com>
2022-10-13 14:05:24 +0200

    "new look"

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
 import { FirebaseEntity } from "./firebase-entity";
 import { generatePaypalLink, getParam } from "./utils";
 
-async function initApp(){
-  setTimeout(async () => {
-    const fEntity = new FirebaseEntity()
-    const id = getParam('id');
-    const link = await fEntity.getLinkById(id);
-    const user = await fEntity.getUserById(link.user_id);
-    const paypalLink = generatePaypalLink(id, link, user);
-    document.getElementById('app').innerHTML = link.status === 'completed' ? createLinkContainer(link) 
-                                                                           : createPaypalContainer(link, user, paypalLink)
-  }, 3000);
+async function initApp() {
+
+  const fEntity = new FirebaseEntity()
+  const id = getParam('id');
+  const link = await fEntity.getLinkById(id);
+  const user = await fEntity.getUserById(link.user_id);
+  const paypalLink = generatePaypalLink(id, link, user);
+  if (link.status === 'completed') {
+    redirectToREV(link);
+  } else {
+    document.getElementById('app').innerHTML = createPaypalContainer(link, user, paypalLink)
+  }
+
 
 }
 
-function createPaypalContainer(link, user, paypalLink){
+function createPaypalContainer(link, user, paypalLink) {
   return `
     <div class="link-container" id="link-container">
-      <span>Ciao ${link.name}</span>
-      <span>il dottor ${user.name} ti ha inviato una ricetta</span>
-      ${link.pet ? `<span>per ${link.pet}</span>`: ''}
-      ${link.comment ? `<span>ed ha aggiunto queste informazioni:</span><span>${link.comment}</span>`: ''}
-      <span>il costo della ricetta è: ${link.price} €</span>
+      <div class="intro">
+        <span class="app-title">REVers€</span>
+        <span>Ciao <b>${link.name}</b>,</span>
+        <span>il dottor <b>${user.name}</b> ti ha inviato una ricetta</span>
+        ${link.pet ? `<span>per <b>${link.pet}</b></span>` : ''}
+        ${link.comment ? `<span>ed ha aggiunto queste informazioni:</span><span>${link.comment}</span>` : ''}
+      </div>
+      <span>il costo della ricetta è:</span>
+      <span class="price"><b>${link.price} €</b></span>
       <span>per effettuare il pagamento clicca qui:</span>
-      <a href="${paypalLink}" target="_blank">Paypal</a>
+      <a href="${paypalLink}" target="_blank" class="paypal-link"><img class="paypal-btn" src="./assets/paypal.svg" alt="bottone pagamento"></a>
     </div>
   `
 }
 
 
-function createLinkContainer(link){
-  return `
-    <div class="link-container" id="link-container">
-      <a href="${link.hidden_link}"><span class="open-link">Accedi alla ricetta</span></a>
-    </div>
-  `
+function redirectToREV(link) {
+  window.location.href = link.hidden_link;
 }
 
 initApp();
